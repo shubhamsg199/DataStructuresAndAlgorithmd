91e805ec579bf09ca20ade519016f15d42f80dc6
Shubham Ganar <shubhamsg123m@gmail.com>
2021-08-06 22:51:16 +0530

    Updated: check Ip address is valid or not

---
+++ Check IP address is valid or not.py
@@ -0,0 +1,37 @@
+
+"""
+Checking valid Ip Address.
+An IP address is a unique address that identifies a device on the internet or a local network.
+A valid IP address must be in the form of A.B.C.D, where A,B,C and D are numbers from 0-254
+
+for example: 192.168.23.1, 192.254.254.254, 192.168.76.45 are valid IP address
+             192.168.255.0, 255.192.3.121, 192.0.1.255 are Invalid IP address
+"""
+def check_valid_IP(ip):
+    """
+    print "Valid IP adddress" if IP is valid or Invalid IP address Invalid
+
+    >>> check_valid_IP(192.168.0.23)
+    192.168 is an valid IP address
+
+    >>> check_valid_IP(192.255.15.8)
+    192.168 is an valid IP address
+    """
+    ip1 = (ip.replace(".", " "))
+    list1 = [int(i) for i in ip1.split() if i.isdigit()]
+    count = 0
+    for i in list1:
+        if i > 254:
+            count += 1
+            break
+    if count:
+        return "Invalid"
+    else:
+        return "Valid"
+if __name__ == "__main__":
+    ip = input()
+    output = check_valid_IP(ip)
+    if output == "Invalid":
+        print(f"{ip} is an {check_valid_IP(ip)} IP address")
+    else:
+        print(f"{ip} is a {check_valid_IP(ip)} IP address")
